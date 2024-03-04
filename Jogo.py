9dc2ea068ca73627e3a21dc6cf5d4f33097eaf42
Alex Sousa <94641250+alexsousa024@users.noreply.github.com>
2024-03-04 22:09:17 +0000

    Update Jogo.py

--- Jogo.py
+++ Jogo.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pygame
 import sys
-from algoritmos import Connect4Game 
+#from algoritmos import Connect4Game 
 import copy 
 
 # Initialize Pygame
@@ -81,9 +81,11 @@ class Board:
     def print_board(self):
 
         print(self.board)
+    
+   
 
 
-    #Escolher a peca
+
 
 def draw_board(board):
     for c in range(COL_COUNT):
@@ -102,20 +104,153 @@ def draw_board(board):
 
 board = Board()
 
-IA = Connect4Game(1,2,board.board)
 
-#Algoritmo A* 
-def generate_sucessors(self):
-    sucessors = []
-    for col in range(COL_COUNT):
-        if self.board.valid_col(col):
-            new_board = copy.deepcopy(self.board)
-            new_board.drop_pieces(self.turn, col)
-            sucessors.append(new_board)
-    return sucessors
 
-#Funcao da heuristica #1 
 
+def Scores(window):
+        score = 0
+        # Contar vitórias absolutas
+    
+        #Mudar para float pq é como está o array numpy 
+        player_1 = 1.0
+        player_2 = 2.0
+        if np.count_nonzero(window == player_2) == 4:
+            score += 5120  # Vitória absoluta para o player_2 
+            
+        elif np.count_nonzero(window == player_1) == 4:
+            
+            score -= 5120  # Vitória absoluta para o player_1
+
+        # Adaptação dos demais cálculos usando np.count_nonzero
+        if np.count_nonzero(window == player_1) == 3 and np.count_nonzero(window == player_2) == 0: 
+            score -= 500
+        elif np.count_nonzero(window == player_1) == 2 and np.count_nonzero(window == player_2) == 0: 
+            score -= 100
+        elif np.count_nonzero(window == player_1) == 1 and np.count_nonzero(window == player_2) == 0: 
+            score -= 10
+        # Não é necessário tratar o caso de ambos 0, pois o score não muda
+        elif np.count_nonzero(window == player_1) == 0 and np.count_nonzero(window == player_2) == 1: 
+            score += 10
+        elif np.count_nonzero(window == player_1) == 0 and np.count_nonzero(window == player_2) == 2: 
+            score += 100
+        elif np.count_nonzero(window == player_1) == 0 and np.count_nonzero(window == player_2) == 3: 
+            score += 500
+
+        return score
+
+
+def board_evaluation(board,player_1,player_2):
+        board_score_matrix = np.array([
+            [3, 4, 5, 7, 5, 4, 3],
+            [4, 6, 8, 10, 8, 6, 4],
+            [5, 8, 11, 13, 11, 8, 5],
+            [5, 8, 11, 13, 11, 8, 5],
+            [4, 6, 8, 10, 8, 6, 4],
+            [3, 4, 5, 7, 5, 4, 3]
+        ])
+        
+        player_score = 0 
+        
+        # Iterar sobre o tabuleiro e calcular o score baseado em posições ocupadas
+        for r in range(ROW_COUNT):
+            for c in range(COL_COUNT):
+                if board[r][c] == player_1:  # Posição ocupada pelo jogador 1
+                    player_score -= board_score_matrix[r][c]
+                elif board[r][c] == player_2:  # Posição ocupada pelo jogador 2
+                    player_score += board_score_matrix[r][c]
+        
+        return player_score
+
+    
+            
+    #Funcap de heurística #1 - Jananlas de 4 em 4 
+def evaluate_function_1(board):
+        score = 0
+        
+        #horizontalmente
+        for r in range(ROW_COUNT):
+            for c in range(COL_COUNT - 3): 
+                window = board[r][c:c+4]
+                
+                score += Scores(window)
+        
+    
+        #verticalmente
+        for r in range(ROW_COUNT-3):
+            for c in range(COL_COUNT): 
+                window = np.array([board[r+i][c] for i in range(4)])
+                
+                score += Scores(window)
+
+        #diagonalente com declive negativo 
+        for r in range(ROW_COUNT-3):
+            for c in range(COL_COUNT-3):
+                window = np.array([board[r+i][c+i] for i in range(4)])
+                score += Scores(window)
+
+    # diagonalmente com declive positivo 
+        for r in range(ROW_COUNT-3):
+            for c in range(COL_COUNT-1, 2, -1):
+                window = np.array([board[r+i][c-i] for i in range(4)])
+                score += Scores(window)
+
+        return score
+    
+def final_heuristic_1(board,player_1, player_2):
+
+        eval_score = evaluate_function_1(board)
+        board_score = board_evaluation(board,player_1,player_2)
+        total_score = eval_score + board_score
+        return total_score
+
+def astar_algorithm(board, player):
+    open_list = [(0, board, None)]  # Custo inicial, estado inicial, e nenhuma jogada ainda
+    best_score = float('-inf')  # Inicializa a melhor pontuação como infinito negativo
+    best_move = None  # Melhor movimento ainda não foi encontrado
+
+    while open_list:
+        _, current_board, move = open_list.pop(0)  # Remove o item com menor custo heurístico
+        # Verifica se o movimento atual é melhor do que o melhor encontrado até agora
+       
+        current_score = final_heuristic_1(current_board.board,1,2)
+        
+        print(current_score)
+        if current_score > best_score:
+            best_score = current_score
+            best_move = move
+
+        # Se o tabuleiro atual representa um estado de vitória, não precisamos continuar
+
+
+        # Gera os sucessores do estado atual
+        successors = generate_sucessors(current_board, player)
+        print(successors)
+
+        for successor, succ_move in successors:
+            # Calcula o custo heurístico para o sucessor
+            print(successor)
+            heuristic = final_heuristic_1(successor,1, player)
+            print(heuristic)
+            # Adiciona o sucessor à lista aberta
+            open_list.append((heuristic, successor, succ_move))
+
+        # Ordena a lista pelo custo heurístico para garantir que o próximo estado a ser explorado é o de menor custo
+        open_list.sort(key=lambda x: x[0])
+        a,b,c = open_list.pop(0)
+
+        return c
+    # Retorna a coluna do melhor movimento encontrado
+    return best_move
+
+
+def generate_sucessors(board,player):
+        sucessors = []
+        for col in range(COL_COUNT):
+            if board.valid_col(col):
+                new_board = copy.deepcopy(board)
+                new_board.drop_pieces(player, col)
+                sucessors.append((new_board.board,col))
+        return sucessors
 
 # Main game loop
 while not board.game_over:
@@ -126,19 +261,21 @@ while not board.game_over:
         if event.type == pygame.MOUSEBUTTONDOWN:
             x_pos = event.pos[0]
             col = int(x_pos // SQUARESIZE)
-            col2 = IA.astar_algorithm()
+            
 
             if board.turn == 0:
                 if board.drop_pieces(1, col):
-                    print(IA.final_heuristic_1(1,2))
+                    print(final_heuristic_1(board.board,1,2))
                     board.print_board() 
                     if board.win(1):
                         print("Player 1 wins!")
                         board.game_over = True
                     board.turn = 1 - board.turn  # Switch turns
             else:
+                col2 = astar_algorithm(board,2)
+                print(col2)
                 if board.drop_pieces(2, col2):
-                    print(IA.final_heuristic_1(1,2))
+                    print(final_heuristic_1(board.board,1,2))
                     board.print_board()
                     if board.win(2):
                         print("Player 2 wins!")
@@ -155,8 +292,5 @@ while not board.game_over:
 pygame.time.wait(3000)  # Delay to see the final board state before closing
 pygame.quit()
 
-
-pygame.time.wait(3000)  # Delay to see the final board state before closing
-pygame.quit()
 
 
